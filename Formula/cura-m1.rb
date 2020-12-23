72f4387439568a4c96853741f2c3c40250d45344
Joe Rocklin <joe.rocklin@gmail.com>
2020-12-23 11:02:50 -0500

    initial work on cura for m1

---
+++ Formula/cura-m1.rb
@@ -0,0 +1,40 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CuraM1 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Ultimaker/Cura"
+  version ""
+  sha256 ""
+  license ""
+
+  depends_on "cmake" => :build
+  
+  depends_on "python"
+  depends_on "qt"
+  depends_on "sip"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cura-m1`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
